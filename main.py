50ee5d4dfb2458a48b90c75f783206b4c6bf702c
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2021-12-14 18:03:31 +0100

    try part of the main file

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import configparser
 import numpy as np
 import cv2
 import glob
+import preparation_data
 
-
+'''
 def parse_args ():
     description = 'Correction of rotation axis for tomographic projections'
     parser = argparse.ArgumentParser(description=description)
@@ -28,49 +29,45 @@ def parse_args ():
                          action= 'store_true',
                          required=False,
                          help='the offset and the tilt angle of the rotation axis is found with respect to the ccentral axis of the detector')
+    args = parser.parse_args()
+    return args
+'''
+def main ():
+    config = configparser.ConfigParser()
+    config.read('paths.ini')
+
+    #args = parse_args()
+
+
+    filepath = config.get('directories','dirpath_tomo')+'/*.tiff'   #path to the tomographic projections
+    flatpath = config.get('directories','dirpath_flat')+'/*.tiff'   #path to the flat image/es
+    darkpath = config.get('directories','dirpath_dark')+'/*.tiff'   #path to the dark image/es
+    last_angle = config.getint('angle','angle')
+
+    print(last_angle,filepath,flatpath,darkpath)
+
+    #lists of images
+    tomo_list = preparation_data.reader_gray_images(filepath)
+    flat_list = preparation_data.reader_gray_images(flatpath)
+    dark_list = preparation_data.reader_gray_images(darkpath)
+
+    print(len(tomo_list),len(flat_list),len(dark_list))
+
+    #3D arrays of images
+    tomo_stack = preparation_data.create_array(tomo_list,tomo_list)
+    flat_stack = preparation_data.create_array(flat_list,tomo_list)
+    dark_stack = preparation_data.create_array(dark_list,tomo_list)
+
+    print(tomo_stack.shape,flat_stack.shape,dark_stack.shape)
+
+if __name__ == '__main__':
+    main()
 
 
 
-config = configparser.ConfigParser()
-config.read('paths.ini')
-
-   #create an empty image list
-
-filepath = config.get('directories','dirpath_tomo')+'/*.tiff'   #path to the tomographic projections
-flatpath = config.get('directories','dirpath_flat')+'/*.tiff'   #path to the flat image/es
-darkpath = config.get('directories','dirpath_dark')+'/*.tiff'   #path to the dark image/es
-last_angle = config.getint('angle','angle')
-
-def reader_gray_images (filepath):
-    global image_list
-    image_list = []
-    for filename in glob.glob(filepath): 
-        im=cv2.imread(filename,0) #0 stands for the gray_image reading (default is RGB reading)
-        image_list.append(im)
-    return image_list
-#create an array for the stack of tomographic projections
-
-projections = np.asarray(image_list)
-
-#create an array for the stack of flat images and one for the dark images
-#the falt_stack has the same dimensions of the projections stack, hence if you have one flat image, it is replicated to fill the ndarray
-#the same for the dark_stack
 
 
 
-#define tomographic projections at angle 0 and 180 degrees
-#projection_180 is the method to find the projection at 180 degree when the last angle is 180 or 360 degrees
-def projection_180 (angle):
-    if angle == 360:
-        proj_180 = projections[int(projections.shape[0]/2),:,:]
-        return proj_180
-    elif angle == 180:
-        proj_180 = projections[int(projections.shape[0]),:,:]
-        return proj_180
-
-
-proj_0 = projections[0,:,:]
-proj_180 = projection_180(last_angle)
 
 
 
