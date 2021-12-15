e1dc385296021063fa41e463c46afb0fcb7e0504
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2021-12-15 10:24:54 +0100

    correction cmap='gray' to show gray-scaled images

--- image_slicer.py
+++ image_slicer.py
@@ -23,7 +23,7 @@ class IndexTracker:
         self.cols  = X.shape[2]
         self.ind = self.slices//2
 
-        self.im = ax.imshow(self.X[self.ind, :, :])
+        self.im = ax.imshow(self.X[self.ind, :, :],cmap='gray')
         self.update()
 
     def on_scroll(self, event):
@@ -41,18 +41,9 @@ class IndexTracker:
 
 
 
-
-image_list = []
-file_path = "C:/Users/Naomi/pythonproj/Tomografia_Darth_Vader/*.tiff" #sys.argv[0]
-
-for filename in glob.glob(file_path): #assuming tiff
-    im=cv2.imread(filename,0) #0 stands for the gray_image reading (default is RGB reading)
-    image_list.append(im)
-
-new_image_list = np.asarray(image_list)
-
-fig, ax = plt.subplots(1, 1)
-tracker = IndexTracker(ax, new_image_list)
-fig.canvas.mpl_connect('key_press_event', tracker.on_scroll)
-plt.show()
+def plot_tracker (img_array):
+    fig, ax = plt.subplots(1, 1)
+    tracker = IndexTracker(ax, img_array)
+    fig.canvas.mpl_connect('key_press_event', tracker.on_scroll)
+    plt.show()
 
