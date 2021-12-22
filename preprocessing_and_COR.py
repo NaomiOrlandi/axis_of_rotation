90648db381ff297c170082edac28ea9de42ebe23
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2021-12-22 13:05:34 +0100

    preprocessing modified (no find_COR)

--- preprocessing_and_COR.py
+++ preprocessing_and_COR.py
@@ -14,6 +14,14 @@ def draw_ROI (proj_0,datapath):
     file_data.close()
     return rowmin,rowmax,colmin,colmax
 
+def cropping (img_stack,rowmin,rowmax,colmin,colmax):
+    if rowmin <= rowmax and colmin <= colmax:
+        for i in range(img_stack.shape[0]):
+            img_stack_cropped = img_stack[:,rowmin:rowmax,colmin:colmax].astype(np.float32)
+        return img_stack_cropped
+    else:
+        raise ValueError ('rowmin and colmin must be less than rowmax and colmax rispectively')
+
 def normalization_with_ROI (img_stack,dark_stack,flat_stack,rowmin,rowmax,colmin,colmax):
     ROI_coor = (rowmin,rowmax,colmin,colmax)
     img_stack_norm = ntp.normalize_proj(img_stack,dark_stack,flat_stack,dose_draw=False,crop_coor=ROI_coor,crop_draw=False)
@@ -44,9 +52,9 @@ def outliers_filter (img_stack, radius_2D_neighborhood, axis=0, k=1.0, out=None)
     #img_stack_filtered = ntp.remove_outliers_stack(img_stack,radius_2D_neighborhood,'local',axis=0,outliers='dark',k=1)
     #return img_stack_filtered
 
-def find_center_of_rotation (proj_0, proj_180, nroi=None, ref_proj=None, ystep=5, ShowResults=False):
-    middle_shift,theta = ntp.find_COR(proj_0, proj_180, nroi=None, ref_proj=None, ystep=5, ShowResults=False)
-    return middle_shift,theta
+#def find_center_of_rotation (proj_0, proj_180, nroi=None, ref_proj=None, ystep=5, ShowResults=False):
+#    middle_shift,theta = ntp.find_COR(proj_0, proj_180, nroi=None, ref_proj=None, ystep=5, ShowResults=False)
+#    return middle_shift,theta
 
 def correct_images (img_stack, proj_0, proj_180,datapath, show_opt='zero', shift=None, theta=None, nroi=None, ystep=5):
     img_stack_corrected,shift,theta = ntp.correction_COR(img_stack, proj_0, proj_180, show_opt='zero', shift=None, theta=None, nroi=None, ystep=5)
@@ -59,8 +67,5 @@ def correct_images (img_stack, proj_0, proj_180,datapath, show_opt='zero', shift
 def save_images (new_fname,img_stack,digits):
     ntp.write_tiff_stack(new_fname,img_stack, axis=0, start=0, croi=None, digit=digits, dtype=None, overwrite=False)
 
-def cropping (img_stack,rowmin,rowmax,colmin,colmax):
-    for i in range(img_stack.shape[0]):
-        img_stack_cropped = img_stack[:,rowmin:rowmax,colmin:colmax].astype(np.float32)
-    return img_stack_cropped
+
 
