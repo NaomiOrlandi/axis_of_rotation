0f0b96f40c942868fb928db06c6f4089e3824254
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2022-01-04 18:46:12 +0100

    documentation added

--- image_slicer.py
+++ image_slicer.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 
 
-
-
-
 class IndexTracker:
     def __init__(self, ax, X):
         self.ax = ax
@@ -19,7 +16,10 @@ class IndexTracker:
         self.update()
 
     def on_scroll(self, event):
-        #print("%s" % (event.key))
+        '''
+        This method allows to scroll through the successive image pressing the right arrow key on the keyboard
+        and to scroll through the previous image pressing the left arrow key on the keyboard
+        '''
         if event.key == 'right':
             self.ind = (self.ind + 1) % self.slices
         elif event.key == 'left':
@@ -34,6 +34,15 @@ class IndexTracker:
 
 
 def plot_tracker (img_array):
+    '''
+    This method shows a stack of images with the possibility to 
+    scroll through the images pressing the right or left arrow key on the keyboard
+    
+    Parameters
+    ----------
+    img_array : ndarray
+        three dimensional stack of images
+    '''
     fig, ax = plt.subplots(1, 1)
     tracker = IndexTracker(ax, img_array)
     fig.canvas.mpl_connect('key_press_event', tracker.on_scroll)
