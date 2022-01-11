1f6d00a06927a484a1a42723202f272fb5cb1816
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2022-01-11 16:01:35 +0100

    correction for finding 180 projection

--- COR/preparation_data.py
+++ COR/preparation_data.py
@@ -77,11 +77,11 @@ def create_array (img_list,img_list_tomo):
     tomo_dim = np.array(img_list_tomo[0]).shape
     if im_dim == tomo_dim:
         if len(img_list) == len(img_list_tomo):
-            img_array = np.asarray(img_list)
+            img_array = np.asarray(img_list, dtype=np.float32)
             return img_array
         elif len(img_list) == 1:
             tomo_array = np.asarray(img_list_tomo)
-            img_array = np.full(tomo_array.shape,img_list)
+            img_array = np.full(tomo_array.shape,img_list, dtype=np.float32)
             return img_array
         elif len(img_list) !=1 and len(img_list) != len(img_list_tomo):
             raise ValueError('{0} should contain or 1 image or an amount of images equal to the num of tomographic projections'.format(img_list))
@@ -118,7 +118,7 @@ def projection_0_180 (angle,img_array):
     '''
     proj_0 = img_array[0,:,:]
     if angle == 360:
-        proj_180 = img_array[int((img_array.shape[0]/2)-1),:,:]
+        proj_180 = img_array[int((img_array.shape[0]/2)),:,:]
         return proj_0,proj_180
     elif angle == 180:
         proj_180 = img_array[int(img_array.shape[0]-1),:,:]
