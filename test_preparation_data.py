e5f352bda868a17e522628c8900b57e8a12fdc04
Naomi Orlandi <naomi.orlandi@studio.unibo.it>
2022-01-26 09:49:35 +0100

    test corrected (global strategies)

--- test_preparation_data.py
+++ test_preparation_data.py
@@ -4,7 +4,23 @@ import numpy as np
 import os
 from hypothesis import given
 import hypothesis.strategies as st
-from hypothesis.extra.numpy import arrays
+from hypothesis.extra.numpy import arrays,array_shapes
+
+
+np.random.seed(0)
+
+#angles used in test_proj_0_180_dimensions() and test_projection_0_180_correct_angles()
+angle1=180
+angle2=360
+#stategy for the shape of a 3D array
+shape_3D = array_shapes(min_dims=3,max_dims=3,min_side=1,max_side=10)
+#strategy for the elements of arrays (floats)
+array_elements = st.floats(min_value=0,allow_nan=False, allow_infinity=False)
+#strategy for a 3D array
+array_3D = arrays(float,shape_3D,elements=array_elements,fill=st.nothing())
+#strategy for a 2D array (5x5)
+array_in_list = arrays(float,(5,5),elements=array_elements,fill=st.nothing())
+
 
 
 
@@ -12,36 +28,9 @@ from hypothesis.extra.numpy import arrays
 #PROPERTY TESTING
 #============================================
 
-blacklist = [".", "<", ">", ":", '"', "/", "|", "?", "*"]
-@given(filepath=st.text(st.characters(blacklist_characters=blacklist,min_codepoint=32,max_codepoint=126),min_size=1,max_size=64)) #codepoints include basic Latin from https://en.wikipedia.org/wiki/List_of_Unicode_characters
-def test_reader_gray_images_random_filepath (filepath):
-    '''
-    Test for asserting that the method preparation_data.reader_gray_images(),
-    when the filepath given as input exists and contains .tiff files,
-    returns a non empty list
-
-    Given
-    -----
-    filepath : str
-        a string of min 1 character and max 64 characters defining the filepath eventually 
-        containing .tiff files
-    '''
-    if os.path.isdir(filepath):
-        if (not any(fname.endswith('.tiff')) for fname in os.listdir(filepath)):
-            with pytest.raises(ImportError) as er:
-                preparation_data.reader_gray_images(filepath)
-            assert str(er.value) == 'directory {0} does not contain any .tiff file'.format(filepath)
-        else:
-            assert len(preparation_data.reader_gray_images(filepath)) > 0
-    else:
-        with pytest.raises(ImportError) as e:
-            preparation_data.reader_gray_images(filepath)
-        assert str(e.value) == 'directory {0} does not exist'.format(filepath)
-
-
-@given(list1=st.lists(arrays(dtype=float,shape=(np.random.randint(1,10),np.random.randint(1,10)),elements=st.floats(min_value=0,allow_nan=False, allow_infinity=False),fill=st.nothing()),min_size=1),
-       list2=st.lists(arrays(dtype=float,shape=(np.random.randint(1,10),np.random.randint(1,10)),elements=st.floats(min_value=0,allow_nan=False, allow_infinity=False),fill=st.nothing()),min_size=1))
-def test_create_array_propr(list1,list2):
+@given(list_few_elements=st.lists(array_in_list,min_size=2),
+       list_one_element=st.lists(array_in_list,min_size=1,max_size=1))
+def test_create_array_shape(list_few_elements,list_one_element):
     '''
     Test that asserts that, when creating the 3D array representing
     the stack of images from a list containing one 
@@ -56,33 +45,20 @@ def test_create_array_propr(list1,list2):
 
     Given
     -----
-    list1, list2 : list
+    list_few_elements, list_one_element : list
         lists of arrays of random floats representing list of images. 
     '''
-    if list1[0].shape == list2[0].shape:
-
-        if len(list1) != 1 or len(list1) != len(list2):
-            with pytest.raises(ValueError) as er:
-                preparation_data.create_array(list1,list2)
-            assert str(er.value) == '{0} should contain or 1 image or an amount of images equal to the num of tomographic projections'.format(list1)
-        
-        else:
-            assert preparation_data.create_array(list1,list2).shape[0] == len(list2)
-            
-            for i in range(2):
-                assert preparation_data.create_array(list1,list2).shape[i+1] == list2[0].shape[i]
     
-    else:
-        with pytest.raises(ValueError) as e:
-            preparation_data.create_array(list1,list2)
-        assert str(e.value) == '{0} should contain images with the same dimensions of tomographic projections'.format(list1)
+    
+    assert preparation_data.create_array(list_few_elements,list_few_elements).shape == tuple([len(list_few_elements),list_few_elements[0].shape[0],list_few_elements[0].shape[1]])
+    assert preparation_data.create_array(list_one_element,list_few_elements).shape == tuple([len(list_few_elements),list_few_elements[0].shape[0],list_few_elements[0].shape[1]])
+    
 
 
-@given(angle=st.integers(min_value=0,max_value=360),
-       array=arrays(dtype=float,shape=(np.random.randint(1,10),np.random.randint(1,10),np.random.randint(1,10)),elements=st.floats(min_value=0,allow_nan=False, allow_infinity=False),fill=st.nothing()))
-def test_proj_0_180 (angle,array):
+@given(array=array_3D)
+def test_proj_0_180_dimensions (array):
     '''
-    Test for the inveriance of the dimensions of the 2D images returned
+    Test for the invariance of the dimensions of the 2D images returned
     from the function preparation_data.projection_0_180() that select the
     image acquired at the angle 0° and the one acquired at 180° from 
     the stack of images obtained from a tomography with last angle equal
@@ -90,21 +66,17 @@ def test_proj_0_180 (angle,array):
 
     Given
     -----
-    angle : int
-        random integer between 0 and 360 that represents the last angle of tomographic acquisition
     array : ndarray
        3D array of floats representing a stack of images
     '''
-    if angle == 180 or angle == 360:
-        for i in range(2): #loop over outputs
-            for j in range (2): #loop over dimensions
-                assert array.shape[j+1] == preparation_data.projection_0_180(angle,array)[i].shape[j]
-    else:
-        with pytest.raises(ValueError) as e:
-            preparation_data.projection_0_180(angle,array)
-        assert str(e.value) == 'the maximum angle for the tomography must be 180 or 360 degrees'
 
+
+    for i in range(2):
+        for j in range(2):
+            assert preparation_data.projection_0_180(angle1,array)[i].shape[j] == array.shape[j+1]
+            assert preparation_data.projection_0_180(angle2,array)[i].shape[j] == array.shape[j+1]
     
+
 
 
 
@@ -120,9 +92,10 @@ def test_reader_gray_image_wrong_filepath ():
     preparation_data.reader_gray_images(), does not exist.
     '''
     filepath = 'not\\existing\\path'
-    with pytest.raises(ImportError) as e:
+    with pytest.raises(OSError) as e:
         preparation_data.reader_gray_images(filepath)
     assert str(e.value) == 'directory {0} does not exist'.format(filepath)
+
 
 def test_reader_gray_image_good_data ():
     '''
@@ -149,6 +122,8 @@ def test_create_array ():
     fin_array = preparation_data.create_array(test_ref_list,test_ref_list)
     assert fin_array.shape == test_array.shape
 
+
+
 def test_create_array_one_img ():
     '''
     Test for the preparation_data.create_array() function when the initial
@@ -161,6 +136,8 @@ def test_create_array_one_img ():
     test_list = np.random.rand(1,2,2).tolist()
     fin_array = preparation_data.create_array(test_list,test_ref_list)
     assert fin_array.shape == test_array.shape
+
+
 
 def test_create_array_few_img ():
     '''
@@ -176,6 +153,8 @@ def test_create_array_few_img ():
         preparation_data.create_array(test_list,test_ref_list)
     assert str(e.value) == '{0} should contain or 1 image or an amount of images equal to the num of tomographic projections'.format(test_list)
 
+
+
 def test_create_array_wrong_list ():
     '''
     Test for preparation_data.create_array() function when the dimensions of 
@@ -186,11 +165,12 @@ def test_create_array_wrong_list ():
     '''
     test_ref_list = np.random.rand(3,2,5).tolist()
     test_list = np.random.rand(3,4,2).tolist()
-    ref_dim = np.array(test_ref_list[0]).shape
-    test_dim = np.array(test_list[0]).shape
     with pytest.raises(ValueError) as e:
         preparation_data.create_array(test_list,test_ref_list)
     assert str(e.value) == '{0} should contain images with the same dimensions of tomographic projections'.format(test_list)
+
+
+
 
 def test_projection_0_180_correct_angles ():
     '''
@@ -198,12 +178,13 @@ def test_projection_0_180_correct_angles ():
     of images (3D array) returns the tomogrphic image acquired at 0° (the first image)
     and the one acquired at 180° (the image at the end or in the middle of the stack,
     depending if the last angle of acquisition is 180° or 360°)
-    In this test the angles considered are 180 and 360.
+    The test assert that the two images selected in the stack are corrected.
+    In this test the angles considered are 180 (angle1 )and 360 (angle2).
     '''
     array=np.random.rand(5,2,2)
     array_corr=np.where(np.isnan(array),0,array)
-    arr_360_0,arr_360_180=preparation_data.projection_0_180(360,array_corr)
-    arr_180_0,arr_180_180=preparation_data.projection_0_180(180,array_corr)
+    arr_360_0,arr_360_180=preparation_data.projection_0_180(angle2,array_corr)
+    arr_180_0,arr_180_180=preparation_data.projection_0_180(angle1,array_corr)
     assert np.array_equiv(array_corr[0],arr_360_0)
     assert np.array_equiv(array_corr[2],arr_360_180)
     assert np.array_equiv(array_corr[0],arr_180_0)
